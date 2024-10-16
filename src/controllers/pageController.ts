f879d83499d0d320d5a4ef7031e20ac626708248
Gui <valeguilherme01@gmail.com>
2024-10-16 16:31:45 -0300

    comecei os models

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,46 @@
 import { Request , Response } from "express";
+import { createMenu} from "../helpers/createMenu";
 
 export const home = (req : Request, res: Response) => {
-    res.send("home no controller");
+    res.render("pages/page" ,{
+        menu: createMenu('all'),
+
+        banner: {
+            title: "Todos os animais" ,
+            background: "allanimals.jpg"
+        }
+    })
 }
 
 export const dogs = (req : Request, res: Response) => {
+    res.render("pages/page" ,{
 
+        menu: createMenu('dog'),
+        banner: {
+            title: "Todos os cachorros" ,
+            background: "banner_dog.jpg"
+        }
+    })
 }
 
 export const cats = (req : Request, res: Response) => {
+    res.render("pages/page" ,{
 
+        menu: createMenu('cat'),
+        banner: {
+            title: "Todos os gatos" ,
+            background: "banner_cat.jpg"
+        }
+    })
 }
 
 export const fishes = (req : Request, res: Response) => {
+    res.render("pages/page" ,{
+        menu: createMenu('fish'),
 
+        banner: {
+            title: "Todos os peixes" ,
+            background: "banner_fish.jpg"
+        }
+    })
 }
